b9fea1e2cafe2dd6b4e490ef42c787949e5c2ee7
TerminusMik <79770524+TerminusMik@users.noreply.github.com>
2021-05-13 09:53:57 +0200

    Delete server.lua

--- server.lua
+++
@@ -1,6 +0,0 @@
-ESX = nil
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-
-AddEventHandler('onResourceStart', function(resource)
-    TriggerClientEvent("Tms:Chargement","OpenMenu")
-end)
